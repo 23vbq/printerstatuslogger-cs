92c96207207dc78b5a61705fa8e7b6ce0d420505
23vbq <vblackgp123@gmail.com>
2024-01-02 21:48:22 +0100

    misdissabled alerter

--- PrinterStatusLogger/Program.cs
+++ PrinterStatusLogger/Program.cs
@@ -46,7 +46,7 @@ namespace PrinterStatusLogger
                 Alerter.Initialize(configManager.GetAlerterCreds(), configManager.LoadAlerter);
                 Logger.Log(LogType.INFO, "Starting printers scan");
                 printerManager.RunPrinterScan();
-                //Alerter.Send();
+                Alerter.Send();
             } catch (Exception ex)
             {
                 Logger.Log(LogType.ERROR, ex.Message);
